d719fb74b8e0c52d526fe67c8cd452f864d312fc
scottbishop <scottbishop70@gmail.com>
2015-05-26 14:36:36 -0700

    Added some git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,7 +20,26 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gw='./gradlew --offline'
 
-gitcleanup () {
-    hub branch -d $(gitmerged)
-    hub remote prune origin
+alias git=hub
+alias gitoneline="git log --graph --decorate --pretty=format:\"%C(auto)%h%d %Cblue%an%Creset: %C(auto)%s\""
+alias gitonelineall="git log --graph --decorate --all --pretty=format:\"%C(auto)%h%d %Cblue%an%Creset: %C(auto)%s\""
+alias gitpretty="git log --graph --decorate --name-status"
+alias gitprettyall="git log --graph --decorate --name-status --all"
+alias gitreset="git reset HEAD\^" # convenience function to go back one commit
+alias gitpush="git push origin HEAD"
+alias gitwip="git commit -a -m 'WIP DO NOT COMMIT'"
+
+function gitmergecommit() { git log $1..HEAD --ancestry-path --merges }
+function gitmerged() { git branch --merged $@ | sed -e '/^*/d' }
+function gitcleanup() { git branch -d $(gitmerged); git remote prune origin }
+function gitshowsvn() { git show `git svn find-rev r$1` }
+function gitsvnrebase() {
+	if [[ $1 != "-l" ]]; then
+		git svn fetch;
+	fi
+	git for-each-ref --shell --format='git co %(refname:short); git svn rebase -l;' refs/heads | \
+		while read entry
+		do
+			eval "$entry"
+		done
 }
